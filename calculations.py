3f625c72a296a6468e3731d8275ab3a5f78f8770
bigiraneza-car <b.igiraneza@alustudent.com>
2026-07-25 17:58:13 +0200

    first draft of the validation function, to validate the numbers

--- calculations.py
+++ calculations.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-""" member 3 requirements 04, 05, 06."""
+""" calculations.py member 3 
+requirements 04, 05, 06."""
+
+PADS_PER_YEAR = 240 #estimated considering 4 pads per day * 5 days * 12 months
+
+
+class MissingDataError(ValueError):
+    """Raised when a value needed for the calculations is missing or invalid"""
+
+
+def validate_number(value, field, allow_zero=True):
+    #validate the value and return it as a float
 
